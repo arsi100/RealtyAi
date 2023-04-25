9ad8823d4539b717fa0dbc54a1d05b7270b66cc5
arsi100 <130777740+arsi100@users.noreply.github.com>
2023-04-24 20:53:35 -0400

    Update IndexAppwError.py
    updated last line to include host and port to ensure it passes app runner health check (port 8080 instead of the Flask default 5000)

--- IndexAppwError.py
+++ IndexAppwError.py
@@ -26,4 +26,7 @@ def main():
     return render_template('IndexAppDesign.html', answer=None)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8080)
+
+    
+
